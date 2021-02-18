cdd4b42d7ccca02c85d0f4d62b4531927bcc62fe
Tom Vander Aa <vanderaa@imec.be>
2021-02-18 20:34:43 +0100

    larger numbers

--- create_images.sh
+++ create_images.sh
@@ -6,5 +6,5 @@ rm -f images/*
 for number in 0 1 2 3 4 5 6 7 8 9
 do
     file="images/$number.png"
-    convert -size 100x200 -background white -fill black -gravity Center -pointsize 200 -font "GillSans" caption:"$number" $file
+    convert -size 150x300 -background white -fill black -gravity Center -pointsize 300 -font "GillSans" caption:"$number" $file
 done
