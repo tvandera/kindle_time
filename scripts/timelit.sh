0d1d7fe1e66c1f946e12b2df3e57ec339bc910ea
Tom Vander Aa <vanderaa@imec.be>
2021-02-21 20:13:15 +0100

    WIP: change fontsize & layout, added inf loop

--- scripts/timelit.sh
+++ scripts/timelit.sh
@@ -1,20 +1,24 @@
-#!/mnt/usr/clock/bin/bash
+#!/mnt/us/clock/bin/bash
 
 ROOT=/mnt/us/clock
 FBINK=$ROOT/bin/fbink
 
-# if the Kindle is not being used as clock, then just quit
-test -f /mnt/us/clock/clockisticking || exit
 
-TIME=$(date -R +"%H%M")
+while true
+do
+    # if the Kindle is not being used as clock, then just quit
+    test -f /mnt/us/clock/clockisticking || exit
 
-# clear the screen
-eips -c
+    TIME=$(date -R +"%H%M")
 
-$FBINK -g  "file=$ROOT/images/${TIME:0:1}.png,x=100,y=100"
-$FBINK -g  "file=$ROOT/images/${TIME:1:1}.png,x=300,y=100"
-$FBINK -g  "file=$ROOT/images/${TIME:2:1}.png,x=100,y=400"
-$FBINK -g  "file=$ROOT/images/${TIME:3:1}.png,x=300,y=400"
+    # clear the screen
+    # eips -c
 
+    $FBINK -g  "file=$ROOT/images/${TIME:0:1}.png,x=80,y=20"
+    $FBINK -g  "file=$ROOT/images/${TIME:1:1}.png,x=300,y=20"
+    $FBINK -g  "file=$ROOT/images/${TIME:2:1}.png,x=80,y=380"
+    $FBINK -g  "file=$ROOT/images/${TIME:3:1}.png,x=300,y=380"
 
-echo $TIME >/mnt/us/clock/clockisticking
+    # sleep until next minute
+    sleep $((60 - $(date +%S)))
+done
